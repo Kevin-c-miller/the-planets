efb62a3f4962ab6dc1569881f78800c8365b1a9d
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-01-12 13:38:26 -0500

    styled the planet details component

--- the-planets/src/components/PlanetDetails.jsx
+++ the-planets/src/components/PlanetDetails.jsx
@@ -3,6 +3,7 @@ import { useParams, Link } from 'react-router-dom';
 import {} from 'react-router-dom';
 import api from '../services/apiConfig/planet.js';
 import './PlanetDetails.css';
+import PlanetDetailsCard from './PlanetDetailsCard.jsx';
 
 export default function PlanetDetails() {
   const [planet, setPlanet] = useState({});
@@ -25,68 +26,8 @@ export default function PlanetDetails() {
   }
 
   return (
-    <div className="grid">
-      <div className="planet-container shadow">
-        <div className="planet-picture card">
-          <img
-            style={{ width: '400px', height: '350px' }}
-            src={planet.fields.image}
-            alt={planet.fields.Name}
-          />
-        </div>
-        <div className="planet-info card">
-          <h2>{planet.fields?.Name}</h2>
-
-          <p>
-            <b>Order From Sun:</b> {planet.fields?.orderFromSun}
-          </p>
-
-          <p>
-            <b>Planet Type:</b> {planet.fields?.planetType}
-          </p>
-
-          <p>
-            <b>Distance from the sun:</b> {planet.fields?.distanceFromSun}{' '}
-          </p>
-
-          <p>
-            <b>Length of a day:</b> {planet.fields?.lengthOfDay}
-          </p>
-
-          <p>
-            <b>Length of a year</b>
-            {planet.fields?.lengthOfYear}
-          </p>
-
-          <p>
-            <b>Size:</b> {planet.fields?.size}
-          </p>
-
-          <p>
-            <b>Number of Moons:</b> {planet.fields?.numberOfMoons}
-          </p>
-
-          <p>
-            <b>Average Temperature:</b> {planet.fields?.temperature}
-          </p>
-
-          <p>
-            <b>Atmosphere:</b> {planet.fields?.atmosphere}
-          </p>
-
-          <p>
-            <b>Number of Rings:</b> {planet.fields?.rings}
-          </p>
-
-          <p>
-            <b>Discovered:</b> {planet.fields?.discoveryDate}
-          </p>
-
-          <p>
-            <b>Random Facts:</b> {planet.fields?.randomFacts}
-          </p>
-        </div>
-      </div>
+    <div>
+      <PlanetDetailsCard planet={planet} id={id} />
     </div>
   );
 }
---
+++ the-planets/src/components/PlanetDetailsCard.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+
+export default function PlanetDetailsCard(props) {
+  const { planet } = props;
+  return (
+    <div>
+      <div className="p-5">
+        <div className="row text-center">
+          <div className="text-light">
+            <div className="mb-3">
+              <h2>{planet.fields?.Name}</h2>
+            </div>
+            <div className="planet-img">
+              <img
+                style={{ width: '400px', height: '350px' }}
+                src={planet.fields.image}
+                alt={planet.fields.Name}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="row text-center">
+          <div className="col-md">
+            <div className="card card-planet bg-dark text-light">
+              <div className="h1 mb-3">
+                <i class="bi bi-globe"></i>
+              </div>
+              <div className="card-text">
+                <p>
+                  <b>Order From Sun</b> <br /> {planet.fields?.orderFromSun}
+                </p>
+                <p>
+                  <b>Planet Type:</b> <br /> {planet.fields?.planetType}
+                </p>
+                <p>
+                  <b>Distance from the sun:</b>
+                  <br /> {planet.fields?.distanceFromSun}
+                </p>
+                <p>
+                  <b>Length of a day:</b>
+                  <br /> {planet.fields?.lengthOfDay}
+                </p>
+                <p>
+                  <b>Length of a year</b>
+                  <br />
+                  {planet.fields?.lengthOfYear}
+                </p>
+
+                <p>
+                  <b>Number of Moons:</b>
+                  <br /> {planet.fields?.numberOfMoons}
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md">
+            <div className="card card-planet bg-dark text-light">
+              <div className="card-text">
+                <p>
+                  <b>Size:</b>
+                  <br /> {planet.fields?.size}
+                </p>
+                <p>
+                  <b>Number of Rings:</b>
+                  <br />
+                  {planet.fields?.rings}
+                </p>
+                <p>
+                  <b>Average Temperature:</b>
+                  <br />
+                  {planet.fields?.temperature}
+                </p>
+
+                <p>
+                  <b>Atmosphere:</b>
+                  <br />
+                  {planet.fields?.atmosphere}
+                </p>
+
+                <p>
+                  <b>Discovered:</b> <br />
+                  {planet.fields?.discoveryDate}
+                </p>
+
+                <p>
+                  <b>Random Facts:</b>
+                  <br /> {planet.fields?.randomFacts}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
